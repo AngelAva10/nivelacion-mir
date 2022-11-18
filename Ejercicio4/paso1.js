28786c7d163035e2f6d6bf4a3ab2dadb56b4a731
Angel <angelviveros92@gmail.com>
2022-11-18 20:55:02 -0300

    ejercicio hola realizado

---
+++ Ejercicio4/paso1.js
@@ -0,0 +1,9 @@
+//Crea una función llamada Hola
+//Retorne una respuesta al saludo
+
+function Hola(cadena) {
+    return "Hola" +" "+ cadena + "!";
+}
+console.log(Hola("Pedro"));
+console.log(Hola("Juan"));
+console.log(Hola(""));
