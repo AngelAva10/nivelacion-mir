48aadb5138002d6a7987010df8556c150ddff696
Angel <angelviveros92@gmail.com>
2022-11-23 22:25:00 -0300

    ejercicio 6 realizado

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,8 +1,29 @@
-const pedro = {
+var pedro = {
     nombre: "Pedro Perez",
     edad: 30,
     activo: true,
-    hobbies:["programar","squash", "piano"]
+    hobbies: ["programar", "squash", "piano"]
+}
+console.log(Object.keys(pedro))
+var items = Object.keys(pedro)
+for (var i = 0; i<items.length; i++) {
+    var item = items[i];
+    console.log(`${item} : ${pedro[item]}`)
+
+} 
+
+pedro.saluda = function saluda() {
+    console.log("Hola, me llamo " + this.nombre);
   }
 
-  console.log(pedro.edad)
+pedro.saluda();
+
+/*
+pedro.estatura = 1.8; 
+delete pedro.activo;
+*/
+/*
+console.log(pedro.edad)
+console.log(pedro.estatura)
+console.log(pedro.activo)
+*/
